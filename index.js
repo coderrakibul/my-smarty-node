994d48b8efee7ad93c7fa19bc55d282fe69ab372
coderrakibul <coder.rakibul@gmail.com>
2022-04-20 21:17:36 +0600

    site is ready

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('hello from my smarty node')
@@ -19,13 +23,21 @@ const users = [
 
 app.get('/users', (req, res) => {
     res.send(users);
-})
+});
 
 app.get('/user/:id', (req, res) => {
     console.log(req.params);
     const id = req.params.id;
     const user = users.find(u => u.id == id);
     res.send(user)
+});
+
+app.post('/user', (req, res) => {
+    console.log('request', req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user);
 })
 
 app.listen(port, () => {
